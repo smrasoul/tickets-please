9bdd2104834d8d456b9bc4c219735101a4d1b215
Rasoul <smrasoul@yahoo.com>
2025-07-09 22:41:05 +0330

    Token based Auth completed

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('api')
     ->prefix('v1') // 'api/v1' is for service providers.
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')
+    ->apiResource('tickets', TicketController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
