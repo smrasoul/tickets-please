dd42d87d26479596e9a8323d7f2623dc80500f84
Rasoul <smrasoul76@gmail.com>
2025-07-13 22:17:47 +0330

    Creating Resources with Post Requests

--- app/Http/Controllers/AuthorTicketsController.php
+++ app/Http/Controllers/AuthorTicketsController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
-
+use App\Models\User;
 class AuthorTicketsController extends Controller
 {
     public function index($author_id, TicketFilter $filter)
@@ -15,4 +16,33 @@ class AuthorTicketsController extends Controller
             ->filter($filter)
             ->paginate());
     }
+
+    public function store(User $author , StoreTicketRequest $request)
+    {
+
+        $models = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $author->id //Implicitly from route model binding.
+        ];
+
+        return new TicketResource(Ticket::create($models));
+
+    }
+
+    // without Route model binding (Explicit)
+//    public function store($author_id , StoreTicketRequest $request)
+//    {
+//
+//        $models = [
+//            'title' => $request->input('data.attributes.title'),
+//            'description' => $request->input('data.attributes.description'),
+//            'status' => $request->input('data.attributes.status'),
+//            'user_id' => $author_id
+//        ];
+//
+//        return new TicketResource(Ticket::create($models));
+//
+//    }
 }
