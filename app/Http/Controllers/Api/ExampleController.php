f87c2beba35d4a2d6ea92ef83149735f1fdcd526
Rasoul <smrasoul@yahoo.com>
2025-07-20 13:03:59 +0330

    started working on Swagger

---
+++ app/Http/Controllers/Api/ExampleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ExampleController
+{
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Tickets Please OpenApi Documentation",
+     *      description="L5 Swagger OpenApi description",
+     *      @OA\Contact(
+     *          email="admin@admin.com"
+     *      ),
+     *      @OA\License(
+     *          name="Laravel 12.0",
+     *          url="https://laravel.com/docs/12.x/license"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Tickets Please API Server"
+     * )
+
+     *
+     * @OA\Tag(
+     *     name="Projects",
+     *     description="API Endpoints of Projects"
+     * )
+     */
+}
